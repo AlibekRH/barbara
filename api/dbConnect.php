b57914fee0709507072ce2db3b8df4f95664809c
AlibekRH <43960802+AlibekRH@users.noreply.github.com>
2020-03-29 02:05:14 +0300

    Update dbConnect.php

--- api/dbConnect.php
+++ api/dbConnect.php
@@ -1,10 +1,10 @@
 <?php
 
 	//Defining Constants
-	define('HOST','localhost');
-	define('USER','root');
-	define('PASS','');
-	define('DB','fliptable');
+	define('HOST','vlvlnl1grfzh34vj.chr7pe7iynqr.eu-west-1.rds.amazonaws.com');
+	define('USER','ckxg7ecz3m1nnb9k');
+	define('PASS','	myz2kwaqvr01pyc4');
+	define('DB','d5epwpe7jtk4oahw');
 
 	//Connecting to Database
     $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Reach our Servers!');
