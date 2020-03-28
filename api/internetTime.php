36bbc1fda9d8227a16499f32e4e8c79003d1ca93
AlibekRH <43960802+AlibekRH@users.noreply.github.com>
2020-03-28 12:11:54 +0300

    Update internetTime.php

--- api/internetTime.php
+++ api/internetTime.php
@@ -1,5 +1,5 @@
 <?php
-	date_default_timezone_set("Asia/Kolkata");
+	date_default_timezone_set("Asia/Kazakhstan");
 	echo json_encode(
 		["time"=>(date("H").(":").date("i").(" ").date("d").("-").date("m").("-").date("Y"))]);
 ?>
