3acd86ccc582150b9c141f36ccc00a2382c989d5
AlibekRH <43960802+AlibekRH@users.noreply.github.com>
2020-03-30 15:33:30 +0300

    Update getNearbySpots.php

--- api/getNearbySpots.php
+++ api/getNearbySpots.php
@@ -3,16 +3,6 @@
  date_default_timezone_set("Asia/Kazakhstan");
  //Getting the requested
 
- $lat = $_GET['lat'];
- $lng = $_GET['lng'];
- $radius = 1000;
- 
- //Importing database
- require_once('dbConnect.php');
- 
- //Creating sql query with where clause to get an specific employee
- $sql="SELECT *, (3959 * acos(cos(radians('".$lat."')) * cos(radians(lat)) * cos( radians(lng) - radians('".$lng."')) + sin(radians('".$lat."')) * sin(radians(lat)))) AS distance FROM `spots` HAVING distance < 10 ORDER BY distance";
-
  //getting result 
  $r = mysqli_query($con,$sql);
 
