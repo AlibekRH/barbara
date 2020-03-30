09ef85cd7e67574037ae64ed198f3cd31e99ef9d
AlibekRH <43960802+AlibekRH@users.noreply.github.com>
2020-03-30 19:37:02 +0300

    Update internetTime.php

--- api/internetTime.php
+++ api/internetTime.php
@@ -1,5 +1,5 @@
 <?php
-	date_default_timezone_set("Asia/Almaty");
+	date_default_timezone_set('Asia/Almaty');
 	echo json_encode(
 		["time"=>(date("H").(":").date("i").(" ").date("d").("-").date("m").("-").date("Y"))]);
 ?>
